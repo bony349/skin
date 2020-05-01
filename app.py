7b3ae6c73dbd1522186fe50cb5b1627b5eeea93f
MarioMoUrad <44468131+MarioMoUrad@users.noreply.github.com>
2020-05-01 21:43:16 +0200

    test

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import PIL
+import jsonpickle as jsonpickle
+from cv2 import cv2
 
 from helper import *
-from flask import Flask
+from flask import Flask, request, render_template
+
 app = Flask(__name__)
 from PIL import Image
+
 
 @app.route('/')
 def index():
@@ -25,6 +29,28 @@ def index():
         return ("Skin Disease is melanoma")
 
 
+@app.route('/RequestImageWithMetadata', methods=['POST'])
+def post():
+    new_test = []
+    request_data = request.form['some_text']
+    print(request_data)
+    imagefile = request.files.get('imagefile', '')
+    img = Image.open(imagefile)
+    resized_img = img.resize((128, 64), PIL.Image.ANTIALIAS)
+
+    fd_img, hog_img = hog(resized_img, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=True,
+                          multichannel=True)
+    new_test.append(fd_img)
+
+    svm_model = load_model('SVM.sav')
+    prediction = svm_model.predict(new_test)
+    if (prediction == 0):
+        return ("Skin Disease is vitiligo")
+    elif (prediction == 1):
+        return ("Skin Diseas is Psoriasis ")
+    elif (prediction == 2):
+        return ("Skin Disease is melanoma")
+
+
 if __name__ == '__main__':
     app.run()
-
